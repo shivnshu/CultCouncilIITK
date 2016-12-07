dada16facc40e7dd421954302c551b9c5e35d377
shivnshu <shivnshus@gmail.com>
2016-12-07 14:42:15 +0530

    added padding

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,9 @@
 
 function startUp() {
     // Adjust heigt of Left Sidebar
+    $('.topBar').width( $(window).width() + 15);
     $('.leftBar').height( $(window).height() - 170 );
+    $('.bottomBar').width( $(window).width() + 15);
 
 }
 window.onload = startUp;
