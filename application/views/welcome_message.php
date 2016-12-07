f2c30bb7e3f2858d578f904b7f0d8f6eea0c7cd4
shivnshu <shivnshus@gmail.com>
2016-12-07 12:56:42 +0530

    initial with corousel

---
+++ application/views/welcome_message.php
@@ -0,0 +1,105 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>ModCult</title>
+
+	<!-- Included Bootstrap CSS Files -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+
+	<!-- Just include this Css file -->
+	<link rel="stylesheet" href="css/jquery.carousel.fullscreen.css" />
+
+	<link rel="stylesheet" href="css/mystyles.css" />
+
+</head>
+<body>
+
+
+
+	<!-- This is a typical Twitter Bootstrap Carousel -->
+	<!-- Carousel -->
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="5"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="6"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="images/0.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<img src="images/1.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<img src="images/2.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<img src="images/3.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<img src="images/4.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<img src="images/5.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+			<div class="item">
+				<img src="images/6.jpg" alt="" />
+				<div class="container">
+					<div class="carousel-caption">
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+
+	<script src="js/jquery-1.10.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Just include this Js file -->
+	<script src="js/jquery.carousel.fullscreen.js"></script>
+</body>
+</html>
