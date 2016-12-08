17e1634b99fb0c351048b041450eb749d4a8b556
shivnshu <shivnshus@gmail.com>
2016-12-08 11:10:21 +0530

    final touch

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,3 @@
-
-
 	    <!-- Bottom Area START -->
 	    <div class="bottomArea">
 	    <div class="container parent full-width">
@@ -18,6 +16,13 @@
 	<!-- Just include this Js file -->
 	<script src="<?=base_url()?>/js/jquery.carousel.fullscreen.js"></script>
 	<script src="<?=base_url()?>/js/jQuery/jquery.hoverIntent.minified.js"></script>
+
+<!-- For Jsscroll -->
+	<script src="<?=base_url()?>/js/jquery.jscrollpane.css"></script>
+	<script src="<?=base_url()?>/js/jquery.mousewheel.js"></script>
+	<script src="<?=base_url()?>/js/mwheelIntent.js"></script>
+	<script src="<?=base_url()?>/js/jquery.jscrollpane.min.js"></script>
+
 	<script src="<?=base_url()?>/js/myjs.js"></script>
 </body>
 </html>
