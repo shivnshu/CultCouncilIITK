9d1844eb339b8ac1cfa77985c358d92ea20398ca
shivnshu <shivnshus@gmail.com>
2016-12-08 10:31:59 +0530

    Minor refactoring

---
+++ application/views/footer.php
@@ -0,0 +1,23 @@
+
+
+	    <!-- Bottom Area START -->
+	    <div class="bottomArea">
+	    <div class="container parent full-width">
+	        <div class="row">
+						<div class="bottomBar">
+						</div>
+	        </div><!-- END of Row -->
+	      </div><!-- END of Container -->
+	    </div>
+
+	<script src="js/myjs.js"></script>
+	<script src="js/jQuery/jquery.min.js"></script>
+	<!--<script src="js/jquery-1.10.0.min.js"></script> -->
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Just include this Js file -->
+	<script src="js/jquery.carousel.fullscreen.js"></script>
+	<script src="js/jQuery/jquery.hoverIntent.minified.js"></script>
+	<script src="js/myjs.js"></script>
+</body>
+</html>
