3695de9071f2886c3f8e0c80e7ad08fcaca6f89d
Akash Gupta <akash.240897@gmail.com>
2016-12-16 17:50:17 +0530

    els changes

--- application/views/clubs/els.php
+++ application/views/clubs/els.php
@@ -10,11 +10,17 @@
   <h1 class="scroll-title" style="background-color:rgba(157, 18, 18, .7); padding:10px">English Literary Society</h1>
   <div class="scrollBar scroll-pane  col-xs-8" id = "scrollbox">
 
-        <div class="clubs-trans-div" style="width:100%; height:277px;opacity:0"></div>
+        <div class="clubs-trans-div" style="width:100%;opacity:1;margin-bottom: 20px;">
+          <ul class="nav nav-tabs" style="margin-bottom: 20px;">
+                <li class="active"><a href="" data-toggle="tab">ABOUT</a></li>
+                <li><a href="" data-toggle="tab">EVENTS</a></li>
+                <li><a href="" data-toggle="tab">JOIN US</a></li>
+            </ul>
+        </div>
 
         <div class="clubs-content">
-            <p>“There is no surer foundation for a beautiful friendship than a mutual taste in literature.” ― P.G. Wodehouse
-
+            <p>“There is no surer foundation for a beautiful friendship than a mutual taste in literature.” ― P.G. Wodehouse</p>
+            <p>
 Keeping the spirit of the above quote in mind, the English Literary Society (ELS) is a student run club under the Cultural Council, IITK, that seeks to promote literary activities in the campus. We conduct a variety of events, both literary (like various forms of Creative Writing, Word Games and Poetry Slam), and fun (like Just-A-Minute, Dumb Charades and some other experiments) frequently. Indian authors have been invited for judging some of our events and conducting workshops and informal sessions. </p>
         </div>
   </div>
