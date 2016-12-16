77c90c027e5604a299a1da35a95d04f3d080fee9
shivnshu <shivnshus@gmail.com>
2016-12-16 13:06:10 +0530

    fixed scroll height

--- js/myjs.js
+++ js/myjs.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
       $('.leftBar').height( $(window).height() - 50 );
       $('.sub-menu').css('left',($('.leftBar').width()));
       $('.sub-menu').css('top',$('.council-nav').height());
-      $('.scrollBar').height($(window).height()*.62);
+      $('.scrollBar').height($(window).height()*.72);
 
 
       $('#animateit').addClass('animated fadeInLeft'); //example animate.css
@@ -27,7 +27,7 @@ $( document ).ready(function() {
 $( document ).ready(function() {
 	window.onresize = function() {
 		// Adjust heigt of Left Sidebar
-    $('.scrollBar').height($(window).height()*.62);
+    $('.scrollBar').height($(window).height()*.72);
     $('.topBar').width( $(window).width() );
     $('.leftBar').height( $(window).height() - $('.topBar').height() - $('.bottomBar').height() );
     $('.bottomBar').width( $(window).width()  );
