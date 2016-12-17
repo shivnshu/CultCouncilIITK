3120f73d540369d84b64e63e8c9937f20ba786b3
shivnshu <shivnshus@gmail.com>
2016-12-17 11:43:32 +0530

    added exp content els

--- application/views/clubs/els.php
+++ application/views/clubs/els.php
@@ -12,16 +12,31 @@
 
         <div class="clubs-trans-div" style="width:100%;opacity:1;margin-bottom: 20px;">
           <ul class="nav nav-tabs" style="margin-bottom: 20px;">
-                <li class="active"><a href="" data-toggle="tab">ABOUT</a></li>
-                <li><a href="" data-toggle="tab">EVENTS</a></li>
-                <li><a href="" data-toggle="tab">JOIN US</a></li>
+                <li class="active"><a href="#about-els" data-toggle="tab">ABOUT</a></li>
+                <li><a href="#events-els" data-toggle="tab">EVENTS</a></li>
+                <li><a href="#joinus-els" data-toggle="tab">JOIN US</a></li>
             </ul>
-        </div>
+        <!-- </div> -->
 
-        <div class="clubs-content">
-            <p><i>“There is no surer foundation for a beautiful friendship than a mutual taste in literature.” ― P.G. Wodehouse</i>i<br><br></p>
-            <p>
-Keeping the spirit of the above quote in mind, the English Literary Society (ELS) is a student run club under the Cultural Council, IITK, that seeks to promote literary activities in the campus. We conduct a variety of events, both literary (like various forms of Creative Writing, Word Games and Poetry Slam), and fun (like Just-A-Minute, Dumb Charades and some other experiments) frequently. Indian authors have been invited for judging some of our events and conducting workshops and informal sessions. </p>
+        <!-- <div class="clubs-content tab-content"> -->
+        <div class="nav-content">
+
+            <div id="about-els" class="tab-pane fade in active">
+
+                <p><i>“There is no surer foundation for a beautiful friendship than a mutual taste in literature.” ― P.G. Wodehouse</i>i<br><br></p>
+                <p>Keeping the spirit of the above quote in mind, the English Literary Society (ELS) is a student run club under the Cultural Council, IITK, that seeks to promote literary activities in the campus. We conduct a variety of events, both literary (like various forms of Creative Writing, Word Games and Poetry Slam), and fun (like Just-A-Minute, Dumb Charades and some other experiments) frequently. Indian authors have been invited for judging some of our events and conducting workshops and informal sessions. </p>
+            </div>
+
+            <div id="events-els" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Some content in menu 1.</p>
+            </div>
+
+            <div id="joinus-els" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+            </div>
+
         </div>
   </div>
 </div>
