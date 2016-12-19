80792ed4b8686289009c0b2e8b4a6db4d1faa2d1
Akash Gupta <akash.240897@gmail.com>
2016-12-19 14:36:10 +0530

    dram content

--- application/views/clubs/dramatics.php
+++ application/views/clubs/dramatics.php
@@ -7,28 +7,51 @@
 </script>
 
 <div style= "margin-left:32%" class="page-title  col-xs-6 col-xs-offset-3">
-    
+
   <h1 class="scroll-title">Dramatics Club</h1>
 
   <div class="scrollBar scroll-pane  col-xs-8" id = "scrollbox">
 
         <div class="clubs-trans-div" style="width:100%;opacity:1;margin-bottom: 20px;">
-          <ul class="nav nav-tabs" style="margin-bottom: 20px;">
-                <li class="active"><a href="#about-dramatics" data-toggle="tab">ABOUT</a></li>
-                <li><a href="#events-dramatics" data-toggle="tab">EVENTS</a></li>
-                <li><a href="#joinus-dramatics" data-toggle="tab">JOIN US</a></li>
-            </ul>
-        <!-- </div> -->
 
-        <!-- <div class="clubs-content tab-content"> -->
-        <section class="nav-content" style="width:100%; text-align:left;">
+        <ul class="nav nav-tabs" style="margin-bottom: 20px;">
+  <li class="active"><a data-toggle="tab" href="#about-dramatics">About</a></li>
+  <li><a data-toggle="tab" href="#events-dramatics">Events</a></li>
+  <li><a data-toggle="tab" href="#joinus-dramatics">Join Us</a></li>
+</ul>
+
+        <div class="tab-content" style="width:100%; text-align:left;">
 
             <article id="about-dramatics" class="tab-pane fade in active">
-
+                  <p>Ever dreamed about being somebody else? What will be your actions when you become them? As Mr. Noel Coward says <i>‘Acting is not a state of being … but a state of appearing to be.’</i> i.e. acting is the closest way to live a person’s life. </p>
+                  <p>
+                  Welcome to Dramatics Club, IIT Kanpur which provides you the opportunity to satisfy not only your ‘acting ka keeda’ but many other keedas of scriptwriting, direction and backstage along with thrill of ovations as your production reaches its stage.</p>
                
             </article>
 
             <article  id="events-dramatics" class="tab-pane fade">
+                 <p>
+                         <b>(a) Orientation Nukkad:</b>
+                         A Street Play by club seniors for the incoming batch which gives them their first glance of a performance from the club.
+                    </p>
+                <p>
+                         <b>(b) Fresher’s Night:</b>
+                            ‘What happens in Dramatics Club?’ is the first question in the mind of a fresher. Well here is the answer with the first production of club performed solely by students of incoming batch.</p>
+                <p>
+                        <b>(c) Dram Eve-1</b>
+                        The gems of past performances of the club are unearthed for the new batch as second and first years come together to perform some of its earlier street plays.
+                </p>
+                <p>
+                    <b>(d) Antaragni:</b>
+                        Stage Play. Street Play. Mime. The Cultural fest of IIT Kanpur brings forth an exciting chance for competing against incoming colleges.
+                </p>
+                <p>
+                    <b>(e) Dram Eve-2:</b>
+                    Flagship event of the club with all its members putting a dazzling array of plays, nukkad and mime performances for crowd of IIT Kanpur.</p>
+                    <p>
+                    <b>(f) Summer Workshop:</b>
+                    A whole month of learning of elements of stage play under a theatre professional at the end of which a professionally directed play is staged for students and professors alike.
+                 </p>
                 
             </article>
 
